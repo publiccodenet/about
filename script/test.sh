74b34039b2e9980b92f001c9590caad80edf149a
Boris VAN HOYTEMA <boris@publiccode.net>
2019-11-19 16:22:02 +0100

    Add the Markdownlint Gem and test

--- script/test.sh
+++ script/test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+# Lint markdown using the Markdownlint gem with the default ruleset except for the line length rule and the list-prefix rule
+bundle exec mdl -r ~MD013,~MD029 -i -g '.'
+
 # Build the site
 bundle exec jekyll build
 
