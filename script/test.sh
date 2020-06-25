451cd3cab5a46572097b8f1e83dd86001c75c291
Boris VAN HOYTEMA <boris@publiccode.net>
2020-06-25 15:33:41 +0200

    Ignore rate limits
    Twitter and Github can throw 429 error when too many requests are sent.
    This can come from our tests happening concurrently or other testing
    happening on the same IP address.
    
    If a rate limiting error is thrown it can be assumed that this
    resource exists over that it is broken.
    
    Fixes #695

--- script/test.sh
+++ script/test.sh
@@ -13,9 +13,11 @@ bundle exec jekyll build
 # jekyll does not require extentions like HTML
 # ignore edit links to GitHub as they might not exist yet and
 # set an extra long timout for test-servers with poor connectivity
+# ignore request rate limit errors (HTTP 429) which often come from Twitter or GitHub
 # using the files in Jekylls build folder
 bundle exec htmlproofer \
     --assume-extension \
     --url-ignore "/github.com/(.*)/edit/" \
     --typhoeus-config '{"timeout":60,"ssl_verifypeer":false,"ssl_verifyhost":"0"}' \
+    --http_status_ignore "429" \
     ./_site
